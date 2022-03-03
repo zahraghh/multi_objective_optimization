caea4540ff3c5dac731f164a2985dd29c9e20d39
zahraghh <42415558+zahraghh@users.noreply.github.com>
2022-03-03 09:35:11 -0700

    Add files via upload

--- MILP solver test/MILP_solver.py
+++ MILP solver test/MILP_solver.py
@@ -6,7 +6,7 @@ import pandas as pd
 import csv
 import json
 import multi_operation_planning
-from multi_operation_planning import download_windsolar_data, GTI, scenario_generation_operation, uncertainty_analysis_operation,MILP_two_objective,MILP_results_repdays
+from multi_operation_planning import download_windsolar_data, GTI, scenario_generation_operation, uncertainty_analysis_operation_MCMC_std,MILP_two_objective,MILP_two_objective
 editable_data_path =os.path.join(sys.path[0], 'editable_values.csv')
 editable_data = pd.read_csv(editable_data_path, header=None, index_col=0, squeeze=True).to_dict()[1]
 path_test =  os.path.join(sys.path[0])
@@ -21,11 +21,11 @@ if __name__ == "__main__":
     #Do we need to generate scenarios for uncertainties in ...
     #energy demands,solar irradiance, wind speed, and electricity emissions?
     if editable_data['Scenarios Generation/Reduction']=='yes':
-        scenario_generation_operation.scenario_generation_results(path_test)
-        generated_scenario=uncertainty_analysis_operation.UA_operation(int(editable_data['num_scenarios']))
+        #scenario_generation_operation.scenario_generation_results(path_test)
+        generated_scenario=uncertainty_analysis_operation_MCMC_std.UA_operation(int(editable_data['num_scenarios']))
         with open(os.path.join(path_test,'UA_operation_'+str(num_scenarios)+'.json'), 'w') as fp:
             json.dump(generated_scenario, fp)
     #Do we need to perfrom the two stage stochastic programming using MILP solver (GLPK)?
     if editable_data['Perform multi-objective optimization']=='yes':
         print('Perfrom multi-objective optimization of operation planning')
-        MILP_results_repdays.results_repdays(path_test)
+        MILP_two_objective.results_repdays(path_test)
